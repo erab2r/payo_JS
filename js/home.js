97709c90930d1b94a79c7ad31413ae35f7b05473
erab2r <shkera213@gmail.com>
2025-07-11 22:09:04 +0600

    a

--- js/home.js
+++ js/home.js
@@ -1,1 +1,23 @@
-console.log('home added');
+//add money to the account 
+/*
+S-1: add event handler
+prevent page reload after form submit
+s-2: get money to be added to the account balance get the pin number
+s-3: verify the pin number
+*/
+
+// step-1: add an event handler to the add money button inside the form
+document.getElementById('btn-add-money')
+      .addEventListener('click',function(event){
+         // prevent page reload after form submit
+        event.preventDefault();
+        
+        // step-2: get money to be added to the account
+        const addMoneyInput = document.getElementById('input-add-money').value;
+        console.log(addMoneyInput);
+
+
+        const pinNumberInput = document.getElementById('input-pin-number').value;
+        console.log(pinNumberInput);
+
+})
