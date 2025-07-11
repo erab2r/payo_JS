1413de6f3adad1c812a8b6ff258d5a93e1f9279d
erab2r <shkera213@gmail.com>
2025-07-12 00:33:50 +0600

    q

--- js/home.js
+++ js/home.js
@@ -19,5 +19,24 @@ document.getElementById('btn-add-money')
 
         const pinNumberInput = document.getElementById('input-pin-number').value;
         console.log(pinNumberInput);
+        
+        // step-3: verify pin number
+        // wrong way to validate pin number
+        if(pinNumberInput === '1234'){
+            console.log('Adding money to your account');
+             // step-4: get the current balance
+            const balance = document.getElementById('account-balance').innerText;
+            const addMoneyNumber = parseFloat(addMoneyInput);
+            const balanceNumber = parseFloat(balance);
+            const newBalance = addMoneyNumber + balanceNumber;
+            console.log(newBalance);
 
-})
+              // step-6: update the balance in the UI/DOM
+            document.getElementById('account-balance').innerText = newBalance;
+        }
+        else{
+            alert('Failed to add money');
+        }
+
+
+}) 
