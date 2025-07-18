f63da37ed5e49c507aa89c4ca02145c5d9c1d241
erab2r <shkera213@gmail.com>
2025-07-19 03:42:51 +0600

    1

--- js/cashOut.js
+++ js/cashOut.js
@@ -4,13 +4,27 @@ document.getElementById('btn-cash-out')
 
         const cashOut = getInputFieldValueById('input-cash-out');
         const pinNumber = getInputFieldValueById('input-cash-out-pin');
+        if(isNaN(cashOut)){
+            alert('Please enter a valid number for cash out.');
+            return;
+        }
 
         if(pinNumber === 1234){
             const balance = getTextElementValueById('account-balance');
-
-
+            if(cashOut > balance){
+                alert('Insufficient balance for cash out.');
+                return;
+            }
+           
             const newBalance = balance - cashOut;
             document.getElementById('account-balance').innerText = newBalance;
+            //add to transaction history
+            const div = document.createElement('div');
+            div.classList.add('bg-green-300');
+            div.innerHTML =`
+            <h4 class="text-2xl font-semibold">Cash Out</h4>
+            <p class="text-xl">Cash Out Amount: ${cashOut} BDT New Balance ${newBalance}</p>`
+            document.getElementById('transaction-container').appendChild(div);
         }
         else{
             alert('No money for you.')
